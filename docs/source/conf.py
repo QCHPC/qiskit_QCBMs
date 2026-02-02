ad13a2d63fe88d4aa7047764d288533f399cf5cc
nrhawkins <nrhawkins@gmail.com>
2026-02-02 14:37:38 -0800

    Adding Sphinx theme and autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,16 @@
+# This code is a Qiskit QAMP project.
+#
+# (C) Copyright QCHPC: N. Hawkins, J. Plazas, D. Choudhury 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+#
+#
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,20 +19,57 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import inspect
+import os
+import re
+import sys
+from importlib.metadata import version as metadata_version
+
+# The following line is required for autodoc to be able to find and import the code whose API should
+# be documented.
+sys.path.insert(0, os.path.abspath(".."))
+
+
 project = 'Qiskit-QCBMs'
-copyright = '2026, N. Hawkins, J. Plazas, D. Choudhury'
+copyright = '2026, QCHPC: N. Hawkins, J. Plazas, D. Choudhury'
 author = 'N. Hawkins, J. Plazas, D. Choudhury'
-release = '0.1'
+release = metadata_version("qiskit-addon-qcbm")
+description = (
+    "Train a quantum circuit to represent the probability distribution of the input data when measured."
+)
+language = 'en'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme',
+    "matplotlib.sphinxext.plot_directive",
+    "nbsphinx",
+    "reno.sphinxext",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.intersphinx",  
+    "sphinx_copybutton",
+    "sphinx_reredirects",    
+    'qiskit_sphinx_theme',
+#    'sphinx_rtd_theme',  
 ]
 
 templates_path = ['_templates']
-exclude_patterns = []
+
+# Sphinx should ignore these patterns when building.
+exclude_patterns = [
+    "_build",
+    "_ecosystem_build",
+    "_qiskit_build",
+    "_pytorch_build",
+    "**.ipynb_checkpoints",
+    "jupyter_execute",
+]
 
 
 
@@ -27,7 +77,59 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'qiskit-ecosystem'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'qiskit-ecosystem'
+
+# This allows including custom CSS and HTML templates.
+html_theme_options = {
+    "dark_logo": "images/qiskit-dark-logo.svg",
+    "light_logo": "images/qiskit-light-logo.svg",
+    "sidebar_qiskit_ecosystem_member": False,
+}
 
 html_static_path = ['_static']
+templates_path = ["_templates"]
+
+html_last_updated_fmt = "%Y/%m/%d"
+html_title = f"{project} {release}"
+
+# This allows RST files to put `|version|` in their file and
+# have it updated with the release set in conf.py.
+rst_prolog = f"""
+.. |version| replace:: {release}
+"""
+
+# Options for autodoc. These reflect the values from Qiskit SDK and Runtime.
+autosummary_generate = True
+autosummary_generate_overwrite = False
+autoclass_content = "both"
+autodoc_typehints = "description"
+autodoc_default_options = {
+    "inherited-members": None,
+    "show-inheritance": True,
+}
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+# This adds numbers to the captions for figures, tables,
+# and code blocks.
+numfig = True
+numfig_format = {"table": "Table %s"}
+
+# Settings for Jupyter notebooks.
+nbsphinx_execute = "never"
+
+add_module_names = False
+
+modindex_common_prefix = ["qiskit_addon_qcbm."]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "qiskit": ("https://quantum.cloud.ibm.com/docs/api/qiskit/", None),
+    "rustworkx": ("https://www.rustworkx.org/", None),
+}
+
+plot_working_directory = "."
+plot_html_show_source_link = False
+
